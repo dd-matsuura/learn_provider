5b0fba78c1389d7fcbc0f69b588be911c5613e45
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-05-29 01:28:24 +0900

    notifyListenersで値が変わったことを他に知らせて更新させる

--- lib/k.dart
+++ lib/k.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class K extends ChangeNotifier {
   static String k = 'text';
+
+  void TextChanged(String text) {
+    k = text;
+    notifyListeners();
+  }
 }
