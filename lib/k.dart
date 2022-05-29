c044cc7eab14de9ee5eb245bb832773809460c94
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-05-29 23:38:40 +0900

    ChangeNotifierとnotifyListenersの説明追加

--- lib/k.dart
+++ lib/k.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// with ChangeNotifier
+// 変数の状態に変化があれば参照しているwidgetに画面再描画の通知を送る
 class K with ChangeNotifier {
   String k = 'text';
 
   void textChanged(String text) {
     k = text;
+    // 一連の処理が終わったことをwidgetに伝え、画面再描画の通知を送る
     notifyListeners();
   }
 }
